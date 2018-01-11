087b0c07bd455c0eec080b2bab34dcad5019ec54
Oggi Danailov <ogidan@abv.bg>
2018-01-10 22:16:32 -0500

    lines deleted

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 
 var audio = document.getElementById('audio')
 console.log(audio)
-console.log('hellow')
-console.log('whatever')
-console.log('whatever')
-console.log('whatever')
+
 var audio = document.getElementById('audio')
 
 console.log('whatever')
